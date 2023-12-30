9438f873700b978c6fde1bca97f57574b13e2796
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2023-12-31 01:56:36 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 import os
 os.system("pip install flask")
 os.system("pip install playwright")
+os.system("pip install requests")
 os.system("playwright install")
 os.system("playwright install-deps")
