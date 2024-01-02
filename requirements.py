c757c5dc25a93af100b5e8f7fa14c870291507fc
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2024-01-02 15:09:33 +0530

    Delete requirements.py

--- requirements.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("pip install flask")
-os.system("pip install playwright")
-os.system("pip install requests")
-os.system("playwright install")
-os.system("playwright install-deps")
