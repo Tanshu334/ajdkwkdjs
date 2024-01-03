b8198a8fe6ae5feddc22ca45d526667aa6f2f740
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2024-01-03 18:46:35 +0000

    e

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+#API made by very.tuff.no.cap (display name: very tuff cat) on discord.
 import re
 import requests
+import sys
+import os
 from flask import Flask, jsonify, request
 from playwright.sync_api import sync_playwright
 
 app = Flask(__name__)
+browser = None
+context = None
+with sync_playwright() as p: #launching the browser and creating a context to save time.
+    browser = p.chromium.launch()
+    context = browser.new_context()
 
 @app.route("/")
 def index():
@@ -11,13 +19,11 @@ def index():
 
 @app.route("/bypasskey/system/delta", methods=["GET"])
 def delta():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-
+    try:
+        page = context.new_page()
         link = request.args.get("link")
         if not link or "id=" not in link:
-            return jsonify({"status": "Error", "message": "MethodNotAllowed"}), 400
+            return jsonify({"status": "Error", "message": "MethodNotAllowed"}), 404
 
         id = link.split("id=")[1]
 
@@ -45,9 +51,13 @@ def delta():
 
         response = requests.get(f"https://api-gateway.platoboost.com/v1/authenticators/8/{id}")
         data = response.json()
-        browser.close()
 
         return jsonify({"status": "success", "key": data["key"]}), 200
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(f"An error occurred in {fname} on line {exc_tb.tb_lineno}: {e}")
+        return jsonify({"status": "fail", "message": "An error has occured. The developer has been notified and he will fix it ASAP."}), 400
         
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8080")
