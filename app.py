182c7582ce5c28fd7c9070db053efda419bf696e
Tanshu334 <94998344+Tanshu334@users.noreply.github.com>
2024-01-10 19:47:33 +0000

    seg

--- app.py
+++ app.py
@@ -1,63 +1,82 @@
-#API made by very.tuff.no.cap (display name: very tuff cat) on discord.
 import re
 import requests
 import sys
-import os
-from flask import Flask, jsonify, request
+import random
+from flask import Flask, render_template, jsonify, request, redirect
 from playwright.sync_api import sync_playwright
 
 app = Flask(__name__)
-browser = None
-context = None
-with sync_playwright() as p: #launching the browser and creating a context to save time.
-    browser = p.chromium.launch()
-    context = browser.new_context()
 
 @app.route("/")
 def index():
-    return "You aren't supposed to be here. Go away :("
+    return redirect('https://yxubot.vercel.app/')
 
 @app.route("/bypasskey/system/delta", methods=["GET"])
 def delta():
     try:
-        page = context.new_page()
-        link = request.args.get("link")
-        if not link or "id=" not in link:
-            return jsonify({"status": "Error", "message": "MethodNotAllowed"}), 404
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            frames = page.frames
 
-        id = link.split("id=")[1]
+            link = request.args.get("link")
+            if not link or "id=" not in link:
+                return jsonify({"status": "error", "message": "MethodNotAllowed"}), 400
 
-        page.goto(link)
-        page.wait_for_selector("button:text('Continue')")
-        page.get_by_role("button", name="Continue").click(force=True)
-        page.wait_for_url(re.compile(r"^https://linkvertise\.com"))
-        url = page.url
+            id = link.split("id=")[1]
 
-        page.goto("https://adlinkbypass.com/")
-        page.get_by_role("textbox").fill(url)
-        page.get_by_role("button", name="Bypass").click()
-        page.get_by_role("button", name="Copy").click()
+            page.goto(link)
+            for frame in frames:
+                if re.match(r"^https://challenges.cloudflare.com/cdn-cgi", frame.url):
+                    element = frame.locator('.mark')
+                    page.wait_for_timeout(3000)
+                    element.check()
+                    break
+            page.wait_for_selector("button:text('Continue')")
+            page.get_by_role("button", name="Continue").click(force=True)
+            page.wait_for_url(re.compile(r"^https://linkvertise\.com"))
+            url = page.url
 
-        elements = page.query_selector_all("div")
-        for element in elements:
-            text = element.inner_text()
-            if "https://gateway.platoboost.com" in text:
-                url2 = text
+            page.goto("https://adlinkbypass.com/")
+            page.get_by_role("textbox").fill(url)
+            page.get_by_role("button", name="Bypass").click()
+            page.get_by_role("button", name="Copy").click()
 
-        page.goto(url2)
-        page.wait_for_selector("button:text('Continue')")
-        page.get_by_role("button", name="Continue").click(force=True)
-        page.wait_for_timeout(3000)
+            elements = page.query_selector_all("div")
+            for element in elements:
+                text = element.inner_text()
+                if "https://gateway.platoboost.com" in text:
+                    url2 = text
 
-        response = requests.get(f"https://api-gateway.platoboost.com/v1/authenticators/8/{id}")
-        data = response.json()
+            page.goto(url2)
+            page.wait_for_selector("button:text('Continue')")
+            page.get_by_role("button", name="Continue").click(force=True)
+            page.wait_for_timeout(3000)
 
-        return jsonify({"status": "success", "key": data["key"]}), 200
+            response = requests.get(f"https://api-gateway.platoboost.com/v1/authenticators/8/{id}")
+            data = response.json()
+            browser.close()
+
+            return jsonify({"status": "success", "key": data["key"]}), 200
     except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(f"An error occurred in {fname} on line {exc_tb.tb_lineno}: {e}")
-        return jsonify({"status": "fail", "message": "An error has occured. The developer has been notified and he will fix it ASAP."}), 400
+        return jsonify({"status": "error", "message": f"Error occured on line {sys.exc_info()[-1].tb_lineno}:\n{e}"})
+    
+@app.route("api/v1/Bypass/hydrogen")
+def hydrogen():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        link = request.args.get("link")
+
+        if not link or not "method" in link:
+            return jsonify({"status": "error", "message": "Forbidden Method"})
+        
+        page.wait_for_selector("button:text('Get Key')")
+        page.get_by_role("button", name="Get Key").click()
+        page.wait_for_url(re.compile(r"^https://linkvertise\.com"))
+        page.wait_for_timeout(1000)
+        page.go_back()
+
         
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(debug=True, host="0.0.0.0", port=25585)
